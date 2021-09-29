274fde452edd93e7700ee5901fadccdd2bb438ae
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2021-09-29 07:06:07 +0330

    Change dependencies from chromium to chrome

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 Install dependencies:
 
 ```
-apt install -y chromium-browser
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt update -y
+apt install -y google-chrome-stable
 apt install -y libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
 ```
 """
