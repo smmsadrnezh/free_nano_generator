61a3ed4b1e38d666f154f18e0699a3c1d2d53e63
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2021-09-29 06:39:48 +0330

    Add system dependencies

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+Install dependencies:
+
+```
+apt install -y chromium-browser
+apt install -y libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
+```
+"""
+
 import csv
 import time
 import pathlib
@@ -7,7 +16,19 @@ nano_address = "nano_1d8xhr8tj56ee6xhynt3k3jqecetpdznyibx9f531w5pf7rj9oqjkrnxq48
 refresh_rate = 15 * 60
 
 current_path = pathlib.Path(__file__).parent.resolve()
-driver = webdriver.Chrome(current_path / 'chromedriver')  # ChromeDriver 94.0.4606.61
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-setuid-sandbox")
+chrome_options.add_argument("--remote-debugging-port=9222")  # this
+chrome_options.add_argument("--disable-dev-shm-using")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("disable-infobars")
+chrome_options.add_argument(r"user-data-dir=.\cookies\\test")
+driver = webdriver.Chrome(current_path / 'chromedriver', chrome_options=chrome_options)  # ChromeDriver 94.0.4606.61
 driver.get("https://freenanofaucet.com")
 
 i = 0
