74a41c20381fce52b2d5d6bb27011037574b0bf7
Masoud Sadrnezhaad <masoud@sadrnezhaad.ir>
2021-09-29 12:42:59 +0330

    Add beautiful soup to check if it worked properly

--- main.py
+++ main.py
@@ -12,40 +12,52 @@ apt install -y libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1
 
 import csv
 import time
-import pathlib
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
+from bs4 import BeautifulSoup
 
-nano_address = "nano_1d8xhr8tj56ee6xhynt3k3jqecetpdznyibx9f531w5pf7rj9oqjkrnxq48t"
-refresh_rate = 15 * 60
+from settings import url, file_name, refresh_rate, nano_address
 
-current_path = pathlib.Path(__file__).parent.resolve()
 
-options = Options()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-using")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-gpu")
-options.add_argument('--headless')
-options.add_argument("--remote-debugging-port=9222")
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(), options=options)
-driver.get("https://freenanofaucet.com")
+def initiate_chrome_driver():
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-using")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument('--headless')
+    options.add_argument("--remote-debugging-port=9222")
+    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(), options=options)
+    driver.get(url)
+    return driver
 
-i = 0
-with open('log.csv', mode='a') as log_file:
-    while True:
-        i += 1
-        log_line = [i, time.strftime("%H:%M:%S", time.localtime())]
+
+def make_nano(driver):
+    input_element = driver.find_element_by_id("nanoAddr")
+    send_item = driver.find_element_by_id("getNano")
+    input_element.send_keys(nano_address)
+    send_item.click()
+    return driver.page_source
+
+
+def write_logs(html_source):
+    soup = BeautifulSoup(html_source, 'html.parser')
+    element = soup.find_all("p", attrs={"class": "faucetText"})[0]
+    log_row = [time.strftime("%H:%M:%S", time.localtime()), element]
+    with open(file_name, mode='a') as log_file:
         log_writer = csv.writer(log_file, delimiter=',')
-        log_row = [i, time.strftime("%H:%M:%S", time.localtime())]
         log_writer.writerow(log_row)
-        print(log_row)
+    print(log_row)
 
-        input_element = driver.find_element_by_id("nanoAddr")
-        send_item = driver.find_element_by_id("getNano")
-        input_element.send_keys(nano_address)
-        send_item.click()
+
+if __name__ == '__main__':
+
+    driver = initiate_chrome_driver()
+
+    while True:
+        html_source = make_nano(driver)
+        write_logs(html_source)
         time.sleep(refresh_rate)
         driver.back()
---
+++ settings.py
@@ -0,0 +1,5 @@
+nano_address = "nano_1d8xhr8tj56ee6xhynt3k3jqecetpdznyibx9f531w5pf7rj9oqjkrnxq48t"
+refresh_rate = 15 * 60
+
+url = "https://freenanofaucet.com"
+file_name = 'log.csv'
